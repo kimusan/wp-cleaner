dacd467dff3a78959795739ccebdbcf874d53792
Kim Schulz <kim@schulz.dk>
2018-03-02 15:22:32 +0100

    v0.2 cleanup and optimization

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# wp-cleaner
+# wp-cleaner 
+_Kim Schulz <kim@schulz.dk>_
 Simple cleanup/scanner tool for wordpress installations that have been infected
+
+
+simply add to your path and then run the command in the root of your wordpress installation.
+Lines in the output starting with :: will show what pattern the script is currently looking for.
+
+All files possible infected with be listed. It is recommended to check every one of them 
+as there can be "false positives". 
+If all files are backdoor files (new files unrelated to wordpress itself) then you can simply 
+run the script again with --delete as argument. 
+
+## Warning
+It is your own fault if this script causes any damage by deleting important files in your wordpress
+installation. So make sure to make a backup before starting. 
+
+## changelist
+v0.2:
+	* cleanup and fix --delete to work on all platforms. 
+v0.1:
+	* Initial version
--- wp-cleaner.sh
+++ wp-cleaner.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-
-#
-#
-# WORDPRESS BASH SCRIPT TO CLEAN BACKDOOR FILES
-#
-#
+# WORDPRESS CLEANER
+# By Kim Schulz <kim@schulz.dk>
+# Version 0.2
+# github.com/kimusan/wp-cleaner
 FOLDER=.
 if [ "$1" == "--delete" ]; then
 	DELETE=" | tr \"\\n\" \"\\000\" | xargs -0 rm -f"
@@ -12,45 +10,45 @@ else
 	DELETE=""
 fi
 
-echo " :: FilesMan"
+echo " :: Checking for \"FilesMan\":"
 sh -c "grep -rnwl ${FOLDER} -e \"FilesMan\" ${DELETE}"
-echo " :: \"base64_decode\";return > ";
+echo " :: Checking for \"base64_decode\";return\":";
 sh -c "grep -rnwl ${FOLDER} -e \"\\\"base64_decode\\\";return\" ${DELETE}"
-echo " :: ; \$GLOBALS > ";
+echo " :: Checking for \"; \$GLOBALS\":";
 sh -c "grep -rnwl ${FOLDER} -e \"; \\\$GLOBALSi\" ${DELETE}"
-echo " :: <?php \${\" > ";
+echo " :: Checking for \"<?php \${\"\":";
 sh -c "grep -rnwl ${FOLDER} -e \"<?php \\\${\" ${DELETE}"
-echo " :: <?php \$array = array( > ";
+echo " :: Checking for \"<?php \$array = array(\":";
 sh -c "grep -lrnw ${FOLDER} -e \"<?php \\\$array = array(\" ${DELETE}"
-echo " :: mail(stripslashes( > ";
+echo " :: Checking for \"mail(stripslashes(\":";
 sh -c "grep -lrnw ${FOLDER} -e \"mail(stripslashes(\" ${DELETE}"
-echo " :: <?php @array_diff_ukey( > ";
+echo " :: Checking for \"<?php @array_diff_ukey(\": ";
 sh -c "grep -lrnw ${FOLDER} -e \"<?php @array_diff_ukey(\" ${DELETE}"
-echo " :: \$_REQUEST[chr( > ";
+echo " :: Checking for \"\$_REQUEST[chr(\":";
 sh -c "grep -lrnw ${FOLDER} -e \"\\\$_REQUEST\[chr(\" ${DELETE}"
-echo " :: <?php \$GLOBALS[ > ";
+echo " :: Checking for \"<?php \$GLOBALS[\": ";
 sh -c "grep -lrnw ${FOLDER} -e \"<?php \\\$GLOBALS\[\" ${DELETE}"
-echo " :: eval(\$\{ > ";
+echo " :: Checking for \"eval(\$\{\": ";
 sh -c "grep -lrnw ${FOLDER} -e \"eval(\\\${\" ${DELETE}"
-echo " :: isset(\${ > ";
+echo " :: Checking for \"isset(\${\":";
 sh -c "grep -lrnw ${FOLDER} -e \"isset(\\\${\" ${DELETE}"
 
-echo " :: PhpReverseProxy > ";
+echo " :: Checking for \"PhpReverseProxy\": ";
 sh -c "grep -lrnw ${FOLDER} -e \"PhpReverseProxy\" ${DELETE}"
-echo " :: str_rot13 > ";
+echo " :: Checking for \"str_rot13\":";
 sh -c "grep -lrnw ${FOLDER} -e \"str_rot13(\" ${DELETE}"
-echo " :: @set_time_limit(0); > ";
+echo " :: Checking for \"@set_time_limit(0);\": ";
 sh -c "grep -lrnw ${FOLDER} -e \"@set_time_limit(0);\" ${DELETE}"
-echo " :: strripos(@sha1( > ";
+echo " :: Checking for \"strripos(@sha1(\": ";
 sh -c "grep -lrnw ${FOLDER} -e \"strripos(@sha1(\" ${DELETE}"
-echo " :: @assert( >";
+echo " :: Checking for \" @assert(\":";
 sh -c "grep -lrnw ${FOLDER} -e \"@assert(\" ${DELETE}"
-echo " :: made-in-china.com >";
+echo " :: Checking for \"made-in-china.com\":";
 sh -c "grep -lrnw ${FOLDER} -e \"made-in-china.com\" ${DELETE}"
 
-echo " :: trim(curl_exec(\$ch)) >";
+echo " :: Checking for \"trim(curl_exec(\$ch))\":";
 sh -c "grep -lrnw ${FOLDER} -e \"trim(curl_exec(\\\$ch))\" ${DELETE}"
 
 
-echo " :: function.*for.*strlen.*isset > ";
+echo " :: Checking for \"function.*for.*strlen.*isset\": ";
 sh -c "egrep -Rl \"function.*for.*strlen.*isset\" ${FOLDER} ${DELETE}"
